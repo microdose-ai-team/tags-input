2129c1813b343f56fae9cfdca88d4e800359be9d
Justin Bean <justin@microdose.ai>
2025-09-17 15:13:38 -0600

    remove a hidden elem

--- src/tags-input.js
+++ src/tags-input.js
@@ -121,7 +121,6 @@ export default function tagsInput(input, inputID) {
 	input.parentNode[sib?'insertBefore':'appendChild'](base, sib);
 
 	input.style.cssText = 'position:absolute;left:0;top:-99px;width:1px;height:1px;opacity:0.01;';
-	base.style.cssText = 'position:absolute;left:0;top:-99px;width:1px;height:1px;opacity:0.01;';
 	input.tabIndex = -1;
 
 	let inputType = input.getAttribute('type');
