7cc4973d1995291e47ce52506431b19babc61d0d
Justin Bean <justin@microdose.ai>
2025-09-19 16:13:22 -0600

    handle transparency in component

--- src/tags-input.js
+++ src/tags-input.js
@@ -120,7 +120,7 @@ export default function tagsInput(input, inputID) {
 
 	input.parentNode[sib?'insertBefore':'appendChild'](base, sib);
 
-	input.style.cssText = 'position:absolute;left:0;top:-99px;width:1px;height:1px;opacity:0.01;';
+	input.style.cssText = 'position:absolute;left:0;top:-99px;width:1px;height:1px;';
 	input.tabIndex = -1;
 
 	let inputType = input.getAttribute('type');
